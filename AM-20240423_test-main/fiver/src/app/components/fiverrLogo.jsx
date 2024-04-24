df84f1f06a0c599f081d63b86e154b1fe93ce259
feelps04 <81180940+feelps04@users.noreply.github.com>
2024-04-24 14:53:58 +0000

    mais dois

---
+++ AM-20240423_test-main/fiver/src/app/components/fiverrLogo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function FiverrLogo({ fillColor }) {
+  return (
+    <svg
+      width="89"
+      height="27"
+      viewBox="0 0 89 27"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g fill={fillColor}>
+        <path d=""></path>
+      </g>
+      <g fill="#1dbf73">
+        <path d=""></path>
+      </g>
+    </svg>
+  );
+}
+
+export default FiverrLogo;
